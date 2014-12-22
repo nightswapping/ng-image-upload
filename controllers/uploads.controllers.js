6098fe6a221ba21f027273353c2ab84582c3088f
Arnaud Drizard <arnaud.drizard@gmail.com>
2014-12-22 01:01:00 +0100

    throw exception if unkown format

--- controllers/uploads.controllers.js
+++ controllers/uploads.controllers.js
@@ -57,8 +57,10 @@
         name: 'imageFilter',
         fn: function(item /*{File|FileLikeObject}*/, options) {
           var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+          if ('|jpg|png|jpeg|bmp|gif|'.indexOf(type) === -1)
+            throw new Error('File extension not supported (' + type + ')');
+
           return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
-          // TODO: throw error to notice file extension
         }
       });
 
