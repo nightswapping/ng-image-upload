c1b9aae9ada59c215d821465c0b7d30d99198886
Mehdi Benadda <me@mbenadda.com>
2015-04-22 17:29:53 +0200

    Move logic from the directive definition's link function to its controller
    Logic was split arbitrarily between link function and controller, which does
    not seem like good practice. We can make it easier by just moving it to the
    controller and removing the link function entirely as no DOM manipulation
    is involved at all.

--- dist/ng-image-upload.js
+++ dist/ng-image-upload.js
@@ -6,7 +6,8 @@
   uploadsControllers.$inject = [ '$http', '$log', '$sessionStorage', 'uploadsUtils', 'FileUploader' ];
   
   function uploadsControllers ($http, $log, $sessionStorage, utils, FileUploader) {
-    var vm = this;
+    var vm = this,
+        isFileTooBig;
 
     // Don't let the controller get initialized if no token url was provided
     if ((!vm.getTokenUrl || !vm.getTokenUrl()) && !vm.fetchToken) {
@@ -16,12 +17,62 @@
 
     vm.$storage = $sessionStorage;
 
-    var isFileTooBig;
+    // Make sure we can use this callback safely
+    vm.onUploadFinished = vm.onUploadFinished || angular.noop;
 
-    var uploader = vm.uploader = new FileUploader();
+    /**
+     * Initialize and configure a file uploader
+     */
+    vm.uploader = new FileUploader();
 
+    // Set uploader method or default to POST
+    vm.uploader.method = vm.method || 'POST';
+
+    // Set uploader queue limit or default to 1
+    vm.uploader.queueLimit = vm.queueLimit || 1;
+
+    // Remove file from queue, hence from screen, after upload
+    vm.uploader.removeAfterUpload = vm.removeAfterUpload || true;
+
+    // Set up filters to check that images should be uploaded before uploading them
+    // Filters out the items that are not pictures
+    function imageFilter (item /*{File|FileLikeObject}*/, options) {
+      var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+      if ('|jpg|png|jpeg|bmp|gif|'.indexOf(type) === -1) {
+        var err = new Error('File extension not supported (' + type + ')');
+        vm.onUploadFinished(err);
+        throw err;
+      }
+
+      return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+    }
+
+    // Filters out images that are larger than the specified or default limit
+    function sizeFilter (item /*{File|FileLikeObject}*/, options) {
+      var size = item.size,
+          // Use passed size limit or default to 10MB
+          sizeLimit = vm.sizeLimit || 10 * 1000 * 1000;
+      if (size > sizeLimit) {
+        var err = new Error('File too big (' + size + ')');
+        vm.onUploadFinished(err);
+        throw err;
+      }
+      return size < sizeLimit;
+    }
+
+    // Add our new filters to the uploader's filter list if applicable
+    vm.uploader.filters.push({ name: 'sizeFilter', fn: sizeFilter });
+    if (!vm.acceptAllTypes) {
+      vm.uploader.filters.push({ name: 'imageFilter', fn: imageFilter });
+    }
+
+    /**
+     * Define the uploader's callbacks during upload cyclce
+     */
+
+    // angular-file-upload callback. This is fired when a single file is added to the queue.
     // Add the img in session storage once added
-    uploader.onAfterAddingFile = function(fileItem) {
+    vm.uploader.onAfterAddingFile = function(fileItem) {
 
       var canvas = document.createElement('canvas');
 
@@ -46,7 +97,7 @@
         fileItem.file.name = vm.token.filename || fileItem.file.name;
 
         // Url to hit for the post request
-        uploader.url = vm.token.uploadUrl;
+        vm.uploader.url = vm.token.uploadUrl;
         fileItem.url = vm.token.uploadUrl;
 
         // Updates the formData for Amazon AWS S3 Upload
@@ -79,9 +130,8 @@
         canvas.style.visibility = 'hidden';
         document.body.appendChild(canvas);
 
-        uploader.url = vm.token.uploadUrl;
-      })
-      .error(function failure (error) {
+        vm.uploader.url = vm.token.uploadUrl;
+      }, function failure (error) {
         throw new Error(error);
       });
       // Wait for the reader to be loaded to get the right img.src
@@ -92,29 +142,40 @@
       }
     };
 
-    uploader.onBeforeUploadItem = function(fileItem) {
+    // angular-file-upload callback. This is fired before a file is uploaded
+    vm.uploader.onBeforeUploadItem = function(fileItem) {
       // Parse the item stored in session storage
       // before the server upload
-      console.log(fileItem);
       if (!isFileTooBig) {
         fileItem._file = utils.dataURItoBlob(vm.$storage.reader);
       }
     };
 
-    // Post a success message to response url to notify the server
-    // everything went fine
-    uploader.onSuccessItem = function(fileItem, response, status, headers) {
+    // angular-file-upload callback. This is fired when a file is successfully uploaded to the server.
+    // Post a success message to response url to notify the server everything went fine
+    vm.uploader.onSuccessItem = function(fileItem, response, status, headers) {
       if (vm.token.responseUrl) {
         $http.post(vm.token.responseUrl, { filename: fileItem.file.name, response: 'success', status: status, headers: headers });
       }
     };
 
-    // Post an error message to response url to notify the server
-    // something went wrong
-    uploader.onErrorItem = function(fileItem, response, status, headers) {
+    // angular-file-upload callback. This is fired when a file could not be successfully uploaded to the server.
+    // Post an error message to response url to notify the server something went wrong
+    vm.uploader.onErrorItem = function(fileItem, response, status, headers) {
       if (vm.token.responseUrl) {
         $http.post(vm.token.responseUrl, { filename: fileItem.file.name, response: 'error', status: status, headers: headers });
       }
+
+      var err = new Error('Could not upload the picture.');
+      vm.onUploadFinished(err);
+    };
+
+    // angular-file-upload callback. This is fired when an upload is finished, whether it was successful or not.
+    vm.uploader.onCompleteItem = function(fileItem, response, status, headers) {
+      // Empty the session storage once the item has been uploaded
+      delete vm.$storage.reader;
+
+      vm.onUploadFinished(null);
     };
   }
 
@@ -138,7 +199,9 @@
         sizeLimit: '=',
         removeAfterUpload: '=',
         method: '=',
-        onUploadFinished: '=',
+        acceptAllTypes: '=',
+        // onUploadFinished is called when the upload is done. If an error occurred, it is passed as argument
+        onUploadFinished: '=?',
         // fetchToken and tokenUrl coexist as alternatives. Either the user can provide an URL and let us fetch 
         // the token ourselves, or they can hand over a function to do it.
         // This second option is more comprehensive as it allows the user to address errors or special cases directly.
@@ -150,68 +213,7 @@
       },
       controller: 'uploads.controllers',
       controllerAs: 'vm',
-      bindToController: true,
-      link: function(scope, element, attributes) {
-
-        // Don't let the directive get initialized if no token url was provided
-        if (!scope.getTokenUrl()) {
-          throw new Error('img-upload directive must be provided a token-url through the eponymous attribute.');
-        }
-
-        // onUploadFinished is called when the uplad is done
-        // an error is passed as args if an error happened
-        var onUploadFinished = scope.onUploadFinished || function() {};
-
-        scope.uploader.method = scope.method || 'POST';
-        // Only one picture should be uploaded
-        scope.uploader.queueLimit = scope.queueLimit || 1;
-        // File max size accepted
-        var sizeLimit = scope.sizeLimit || 10000000;
-        // Remove file from queue, hence on screen, after upload
-        scope.uploader.removeAfterUpload = scope.removeAfterUpload || true;
-
-        // Filters out the items that are not pictures
-        if (!attributes.hasOwnProperty('acceptAllTypes')) {
-          scope.uploader.filters.push({
-            name: 'imageFilter',
-            fn: function(item /*{File|FileLikeObject}*/, options) {
-              var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
-              if ('|jpg|png|jpeg|bmp|gif|'.indexOf(type) === -1) {
-                var err = new Error('File extension not supported (' + type + ')');
-                onUploadFinished(err);
-                throw err;
-              }
-
-              return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
-            }
-          });
-        }
-
-        scope.uploader.filters.push({
-          name: 'sizeFilter',
-          fn: function(item /*{File|FileLikeObject}*/, options) {
-            var size = item.size;
-            if (size > sizeLimit) {
-              var err = new Error('File too big (' + size + ')');
-              onUploadFinished(err);
-              throw err;
-            }
-            return size < sizeLimit;
-          }
-        });
-
-        scope.uploader.onCompleteItem = function(fileItem, response, status, headers) {
-          // Empty the session storage once the item has been uploaded
-          delete scope.$storage.reader;
-
-          onUploadFinished(null);
-        };
-
-        scope.uploader.onErrorItem = function(fileItem, response, status, headers) {
-          var err = new Error('Couldn\'t not upload the picture');
-          onUploadFinished(err);
-        };
-      }
+      bindToController: true
     };
   }
 
--- src/img-upload/img-upload.ddo.js
+++ src/img-upload/img-upload.ddo.js
@@ -13,7 +13,9 @@
         sizeLimit: '=',
         removeAfterUpload: '=',
         method: '=',
-        onUploadFinished: '=',
+        acceptAllTypes: '=',
+        // onUploadFinished is called when the upload is done. If an error occurred, it is passed as argument
+        onUploadFinished: '=?',
         // fetchToken and tokenUrl coexist as alternatives. Either the user can provide an URL and let us fetch 
         // the token ourselves, or they can hand over a function to do it.
         // This second option is more comprehensive as it allows the user to address errors or special cases directly.
@@ -25,68 +27,7 @@
       },
       controller: 'uploads.controllers',
       controllerAs: 'vm',
-      bindToController: true,
-      link: function(scope, element, attributes) {
-
-        // Don't let the directive get initialized if no token url was provided
-        if (!scope.getTokenUrl()) {
-          throw new Error('img-upload directive must be provided a token-url through the eponymous attribute.');
-        }
-
-        // onUploadFinished is called when the uplad is done
-        // an error is passed as args if an error happened
-        var onUploadFinished = scope.onUploadFinished || function() {};
-
-        scope.uploader.method = scope.method || 'POST';
-        // Only one picture should be uploaded
-        scope.uploader.queueLimit = scope.queueLimit || 1;
-        // File max size accepted
-        var sizeLimit = scope.sizeLimit || 10000000;
-        // Remove file from queue, hence on screen, after upload
-        scope.uploader.removeAfterUpload = scope.removeAfterUpload || true;
-
-        // Filters out the items that are not pictures
-        if (!attributes.hasOwnProperty('acceptAllTypes')) {
-          scope.uploader.filters.push({
-            name: 'imageFilter',
-            fn: function(item /*{File|FileLikeObject}*/, options) {
-              var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
-              if ('|jpg|png|jpeg|bmp|gif|'.indexOf(type) === -1) {
-                var err = new Error('File extension not supported (' + type + ')');
-                onUploadFinished(err);
-                throw err;
-              }
-
-              return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
-            }
-          });
-        }
-
-        scope.uploader.filters.push({
-          name: 'sizeFilter',
-          fn: function(item /*{File|FileLikeObject}*/, options) {
-            var size = item.size;
-            if (size > sizeLimit) {
-              var err = new Error('File too big (' + size + ')');
-              onUploadFinished(err);
-              throw err;
-            }
-            return size < sizeLimit;
-          }
-        });
-
-        scope.uploader.onCompleteItem = function(fileItem, response, status, headers) {
-          // Empty the session storage once the item has been uploaded
-          delete scope.$storage.reader;
-
-          onUploadFinished(null);
-        };
-
-        scope.uploader.onErrorItem = function(fileItem, response, status, headers) {
-          var err = new Error('Couldn\'t not upload the picture');
-          onUploadFinished(err);
-        };
-      }
+      bindToController: true
     };
   }
 
